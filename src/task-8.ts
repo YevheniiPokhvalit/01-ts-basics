5775fba8fe9fff5feffadbe3178a8cfa45f940c0
yevhenii pokhvalit <zpohvalit@gmail.com>
2025-11-20 12:58:26 +0200

    task 8

--- src/task-8.ts
+++ src/task-8.ts
@@ -6,7 +6,7 @@ interface Post {
   body: string;
 }
 
-async function fetchPosts(): Promise<Post[]> {
+async function fetchPosts() {
   const response = await axios.get<Post[]>(
     'https://jsonplaceholder.typicode.com/posts'
   );
